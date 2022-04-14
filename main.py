759096f133590388e9b5772adaa333d9768dcad2
Sharon Bandele <89805354+A00238220@users.noreply.github.com>
2022-04-14 15:10:32 +0000

    added question 4

--- main.py
+++ main.py
@@ -84,3 +84,45 @@ elif input2 == '3':
   tax = 0.13 * (10.99 * int(input3))
   total = tax + (10.99 * int(input3))
   print(f"customer name : {input1.title()}\norder: large-size pizza @ $10.99\ntax: {tax}\ntotal: {total}")
+
+print()
+###########
+#Question 4
+###############
+#Question 3
+#creating a while loop with an input function for age
+count = 0
+
+while True:
+  t_cost = 0
+  count += 1
+  print("\n\nWelcome to Sharon's Restaurant")
+  name = input('Please enter member ' + str(count) + ' name: ')
+  print(f"\nHello, {name.title()}!")
+  age = int(input('Please enter member ' + str(count) + ' age to see dinner cost: '))
+  time.sleep(2)
+
+#creating conditional statements and summary of dinner with updated cost
+  if age < 5:
+    t_cost += 0
+  elif age < 10:
+    t_cost += 5
+  elif age < 18:
+    t_cost += 10
+  elif age > 65:
+    t_cost += 15
+  else:
+    t_cost += 15
+
+  t_cost += t_cost * 0.08
+  avg_cost = t_cost/count
+  cum_cost = t_cost * count
+  
+  print(f"\nSummary:\nYour number of dinner plate is {count}\nYour cost of dinner is ${t_cost}\nYour average dinner cost is ${avg_cost}\nThe cumulative dinner cost is ${cum_cost}")
+
+  member2 = input("\nplease enter 'exit' to end program or press enter to add dinner plate: ")
+  if member2 == 'exit':
+    break
+  else:
+    continue
+  time.sleep(2)
